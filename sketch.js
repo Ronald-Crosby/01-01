a603607e656a8c1b066c276f94ee16633c578010
Olly Robinson <ollythedeveloper@gmail.com>
2020-04-05 10:47:13 +0100

    homework task

--- sketch.js
+++ sketch.js
@@ -1,26 +1,34 @@
 let myFont
 let graphic
+let graphic2
 
 function preload() {
   myFont = loadFont('./spacegrotesk-medium.otf')
 }
 
 function setup() {
-  createCanvas(600, 400);
+  createCanvas(500, 500);
 
-  graphic = createGraphics(600, 400)
+  graphic = createGraphics(500, 500)
   graphic.fill('#ef5236')
   graphic.textAlign(CENTER, CENTER)
   graphic.textFont(myFont)
-  graphic.textSize(300)
-  graphic.text('Olly', width / 2, height / 2)
+  graphic.textSize(400)
+  graphic.text('01', width / 2, height / 2)
+
+  graphic2 = createGraphics(500, 500)
+  graphic2.fill('#ef5236')
+  graphic2.textAlign(CENTER, CENTER)
+  graphic2.textFont(myFont)
+  graphic2.textSize(400)
+  graphic2.text('23', width / 2, height / 2)
 }
 
 function draw() {
   background('#ecf072')
 
-  let val = sin(frameCount * 0.02) * 300
+  let val = sin(frameCount * 0.02) * 250
 
-  copy(graphic, 0, 0, 600, 600, 0, 0, 300 + val, 600)
-  copy(graphic, 0, 0, 600, 600, 300 + val, 0, 300 - val, 600)
+  copy(graphic, 0, 0, 250 + val, 500, 0, 0, 250 + val, 500)
+  copy(graphic2, 250 + val, 0, 250 - val, 500, 250 + val, 0, 250 - val, 500)
 }
